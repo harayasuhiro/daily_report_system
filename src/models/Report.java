ac5034844577c7ededa0d4120e8180a5ee8452de
harayasuhiro <yasu918273@gmail.com>
2020-05-21 20:26:23 +0900

    Add Report index

--- src/models/Report.java
+++ src/models/Report.java
@@ -18,7 +18,7 @@ import javax.persistence.Table;
 @Table(name = "reports")
 @NamedQueries({
         @NamedQuery(name = "getAllReports", query = "SELECT r FROM Report AS r ORDER BY r.id DESC"),
-        @NamedQuery(name = "getReportsCount", query = "SELECT COUNT(r) FROM Employee AS r"),
+        @NamedQuery(name = "getReportsCount", query = "SELECT COUNT(r) FROM Report AS r"),
 
 })
 @Entity
